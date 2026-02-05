5f9906d8d8e22e65881069f3ff4945a4fbf94b02
Dylan Palmboom <nofxboy1234@gmail.com>
2026-02-05 13:40:39 +0200

    Remove unnecessary logger

--- src/components/hooks.ts
+++ src/components/hooks.ts
@@ -1,21 +1,5 @@
-import { useRef, useSyncExternalStore, type DependencyList } from "react";
+import { useSyncExternalStore } from "react";
 import { useCache } from "@/cache";
-
-export function useEffectDepLogger(deps: DependencyList, effectDepsHistory: DependencyList[]) {
-  console.log(deps);
-  effectDepsHistory.push(deps);
-
-  const prev = effectDepsHistory[effectDepsHistory.length - 2]!;
-  const curr = effectDepsHistory[effectDepsHistory.length - 1]!;
-
-  if (effectDepsHistory.length > 1) {
-    console.log(`key dep changed?: ${!Object.is(curr[0], prev[0])}`);
-    console.log(`data dep changed?: ${!Object.is(curr[1], prev[1])}`);
-  } else {
-    console.log("key dep init");
-    console.log("data dep init");
-  }
-}
 
 /**
  * useData now acts as a passive consumer of the cache.
@@ -23,7 +7,6 @@ export function useEffectDepLogger(deps: DependencyList, effectDepsHistory: Depe
  */
 export function useData<T>(key: string) {
   const cache = useCache();
-  const effectDepsHistoryRef = useRef<DependencyList[]>([]);
 
   // Subscribe to cache changes for this specific key
   const data = useSyncExternalStore(
@@ -31,9 +14,6 @@ export function useData<T>(key: string) {
     () => cache.get(key) as T | undefined,
     () => cache.get(key) as T | undefined, // Server snapshot
   );
-
-  // Optional logging for debugging render cycles
-  useEffectDepLogger([key, data], effectDepsHistoryRef.current);
 
   return {
     data,
