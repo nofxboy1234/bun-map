494b78fe87c238b09c68793bab17e9956d75f72e
Dylan Palmboom <nofxboy1234@gmail.com>
2026-03-09 13:55:38 +0200

    Update simple graph

--- src/maps/simple.ts
+++ src/maps/simple.ts
@@ -1,5 +1,13 @@
 import { getPath } from "@/dataFetchers/re9Graph";
 import { graph } from "@/dataFetchers/simpleGraph";
 
-const result = getPath(graph, 0, 2);
+let result;
+
+result = getPath(graph, 0, 2);
+console.log(result);
+
+result = getPath(graph, 2, 0);
+console.log(result);
+
+result = getPath(graph, 2, 2);
 console.log(result);
